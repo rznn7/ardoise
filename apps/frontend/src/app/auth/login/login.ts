2ab0cc9752c3e4b9bf09ebbea6bdee133c3b0455
rznn7 <160644362+rznn7@users.noreply.github.com>
2026-06-11 23:20:11 +0200

    spartan.ng install + login button

--- apps/frontend/src/app/auth/login/login.ts
+++ apps/frontend/src/app/auth/login/login.ts
@@ -1,14 +1,20 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import { lucideKeyRound } from '@ng-icons/lucide';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
 import { AuthService } from 'src/app/auth/auth.service';
 
 type LoginState = { state: 'idle' } | { state: 'loading' } | { state: 'error'; message: string };
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [HlmButtonImports, HlmSpinnerImports, HlmIconImports],
   templateUrl: './login.html',
   styleUrl: './login.css',
+  providers: [provideIcons({ lucideKeyRound })],
 })
 export class Login {
   private readonly router = inject(Router);
